4787da29c1d6efc5224f554b8bd7ba789895b670
adeeba reaz <adeebarivera@outlook.com>
2020-09-01 19:48:03 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ Bundler.require(:default, :development)
 gem 'mail', '~> 2.7', '>= 2.7.1'
 gem 'rack', git: 'https://github.com/rack/rack'
 gem 'nokogiri', :git => 'login@example.com:some-user-account/some-private-gem.git'
+
+source "https://rubygems.org"
+ 
+gem "sinatra"
+ 
+group :development do
+  gem "pry"
+end

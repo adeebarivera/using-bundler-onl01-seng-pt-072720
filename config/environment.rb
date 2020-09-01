b0b3c6ed395f271310a748fb555076ccc08be798
adeeba reaz <adeebarivera@outlook.com>
2020-09-01 19:46:19 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,4 @@ require 'bundler/setup'
 Bundler.require(:default, :development)
 gem 'mail', '~> 2.7', '>= 2.7.1'
 gem 'rack', git: 'https://github.com/rack/rack'
+gem 'nokogiri', :git => 'login@example.com:some-user-account/some-private-gem.git'
